b5e08e1cba4046a7118e09f01ee2e54186658fcb
Goitseone-Themba <goitseonerozthemba@gmail.com>
2026-01-18 00:00:40 +0200

    feat: add splash screen.

--- main.c
+++ main.c
@@ -1,5 +1,13 @@
 #include "raylib.h"
-#include <stdio.h>
+
+typedef enum GameScreen { Splash, GamePlay, Credits } GameScreen;
+
+typedef struct {
+  int posX;
+  int posY;
+  int fontSize;
+  char *text;
+} Text;
 
 typedef struct {
   int id;
@@ -12,6 +20,13 @@ typedef struct {
 
 int main(void) {
 
+  int screenWidth = 800;
+  int screenHeight = 450;
+  Text title = {10, 10, 20, "Sekai"};
+  Text splash = {320, 180, 48, "Loading..."};
+
+  GameScreen currentScreen = Splash;
+
   Base kami = {0, "Entity", "Kami", 100, 100, 50};
   Base ent1 = {1, "Entity", "Musashi Miamoto", 200, 200, 40};
   Base ent2 = {2, "Entity", "Satoshi Nakamoto", 300, 300, 30};
@@ -19,24 +34,45 @@ int main(void) {
 
   Base sekai[4] = {kami, art1, ent1, ent2};
 
-  for (int i = 0; i < 4; i++) {
-    printf("ID: %d\nType: %s\n", sekai[i].id, sekai[i].type);
-  }
+  InitWindow(screenWidth, screenHeight, title.text);
 
-  InitWindow(800, 450, "Sekai");
+  int frameCounter = 0;
 
   SetTargetFPS(60);
 
   while (!WindowShouldClose()) {
-
+    switch (currentScreen) {
+    case Splash: {
+      frameCounter = frameCounter + 2;
+      if (frameCounter > 400) {
+        currentScreen = GamePlay;
+      }
+    } break;
+    default:
+      break;
+    }
     BeginDrawing();
-    ClearBackground(RAYWHITE);
-    DrawText("Sekai", 10, 10, 20, DARKGRAY);
-
-    for (int i = 0; i < 4; i++) {
-      DrawCircle(sekai[i].posX, sekai[i].posY, sekai[i].radius, RED);
-      DrawText(sekai[i].name, sekai[i].posX, sekai[i].posY, sekai[i].radius,
+    switch (currentScreen) {
+    case Splash: {
+      ClearBackground(RAYWHITE);
+      DrawText(splash.text, splash.posX, splash.posY, splash.fontSize,
                DARKGRAY);
+      DrawRectangle(300, 400, frameCounter, 10, GREEN);
+    } break;
+    case GamePlay: {
+
+      ClearBackground(RAYWHITE);
+      DrawText(title.text, title.posX, title.posY, title.fontSize, DARKGRAY);
+
+      for (int i = 0; i < 4; i++) {
+        DrawCircle(sekai[i].posX, sekai[i].posY, sekai[i].radius, RED);
+        DrawText(sekai[i].name, sekai[i].posX, sekai[i].posY, sekai[i].radius,
+                 DARKGRAY);
+      }
+
+    } break;
+    default:
+      break;
     }
 
     EndDrawing();
